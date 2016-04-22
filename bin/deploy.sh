ffe32850b3bd7bfb69a09575a4a1545bf9c95e69
miya0001 <miya@wpist.me>
2016-04-23 04:43:35 +0900

    add auto deploy

---
+++ bin/deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [[ "false" != "$TRAVIS_PULL_REQUEST" ]]; then
+	echo "Not deploying pull requests."
+	exit
+fi
+
+if [[ "master" != "$TRAVIS_BRANCH" ]]; then
+	echo "Not on the 'master' branch."
+	exit
+fi
+
+rm -fr .git
+
+git init
+git config user.name "Travis CI"
+git config user.email "miya+github.com@wpist.me"
+git add api
+git add README.md
+git commit --quiet -m "Deploy from travis"
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
---
+++ bin/prepare.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -e
+
+npm install
+npm run build
