67a5d4ed63cabe95f7d8b7922698dfa240a0fe0f
Toro_Unit (Hiroshi Urabe) <mail@torounit.com>
2016-04-28 19:57:32 +0900

    refactoring gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -7,22 +7,25 @@ var convertEncoding = require( 'gulp-convert-encoding' );
 var chmod = require( 'gulp-chmod' );
 var v1 = require( './lib/v1.js' );
 
-gulp.task( 'download', function () {
+/**
+ * Download and Decompress and Convert Encoding.
+ */
+function getZipCodeCSV() {
   return download( [
     'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip',
   ] )
   .pipe( decompress() )
   .pipe( convertEncoding( { from: "shift_jis", to: "utf-8" } ) )
+}
+
+gulp.task( 'download', function () {
+  return getZipCodeCSV()
   .pipe( chmod( 644 ) )
   .pipe( gulp.dest( 'api' ) );
 } );
 
 gulp.task( 'v1', function () {
-  return download( [
-    'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip',
-  ] )
-  .pipe( decompress() )
-  .pipe( convertEncoding( { from: "shift_jis", to: "utf-8" } ) )
+  return getZipCodeCSV()
   .pipe( v1() )
   .pipe( chmod( 644 ) )
   .pipe( gulp.dest( 'api/v1' ) );
