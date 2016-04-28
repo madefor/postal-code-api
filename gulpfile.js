9de5badad427650e661e2f848b568effb7167750
miya0001 <miya@wpist.me>
2016-04-29 03:18:14 +0900

    separates csv parser

--- gulpfile.js
+++ gulpfile.js
@@ -5,27 +5,30 @@ var download = require( 'gulp-download' );
 var decompress = require( 'gulp-decompress' );
 var convertEncoding = require( 'gulp-convert-encoding' );
 var chmod = require( 'gulp-chmod' );
+var postal2json = require( './lib/postal2json.js' );
 var v1 = require( './lib/v1.js' );
 
 /**
  * Download and Decompress and Convert Encoding.
  */
-function getZipCodeCSV() {
-  return download( [
-    'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip',
-  ] )
+function getZipCodeCSV( urls ) {
+  return download( urls )
   .pipe( decompress() )
   .pipe( convertEncoding( { from: "shift_jis", to: "utf-8" } ) )
 }
 
 gulp.task( 'download', function () {
-  return getZipCodeCSV()
+  return getZipCodeCSV( [
+    'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip',
+    'http://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip'
+  ] )
   .pipe( chmod( 644 ) )
   .pipe( gulp.dest( 'api' ) );
 } );
 
 gulp.task( 'v1', function () {
-  return getZipCodeCSV()
+  return getZipCodeCSV( [ 'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip' ] )
+  .pipe( postal2json() )
   .pipe( v1() )
   .pipe( chmod( 644 ) )
   .pipe( gulp.dest( 'api/v1' ) );
