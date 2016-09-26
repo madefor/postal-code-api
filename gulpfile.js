b65fff84612f97d08a505fe72d3910a4e415eadf
miya0001 <miya@wpist.me>
2016-09-26 20:38:11 +0900

    more readable

--- gulpfile.js
+++ gulpfile.js
@@ -10,37 +10,31 @@ var jigyosyo2json = require( './lib/jigyosyo2json.js' );
 var v1 = require( './lib/v1.js' );
 
 /**
- * Download and Decompress and Convert Encoding.
+ * Create an API of the postal code.
  */
-function getZipCodeCSV( urls ) {
-  return download( urls )
-  .pipe( decompress() )
-  .pipe( convertEncoding( { from: "shift_jis", to: "utf-8" } ) )
-}
-
-gulp.task( 'download', function () {
-  return getZipCodeCSV( [
-    'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip',
-    'http://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip'
-  ] )
-  .pipe( chmod( 644 ) )
-  .pipe( gulp.dest( 'api' ) );
-} );
-
 gulp.task( 'v1', function () {
-  return getZipCodeCSV( [ 'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip' ] )
-  .pipe( postal2json() )
-  .pipe( v1() )
-  .pipe( chmod( 644 ) )
-  .pipe( gulp.dest( 'api/v1' ) );
+  var urls = [ 'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip' ];
+  return download( urls )
+    .pipe( decompress() )
+    .pipe( convertEncoding( { from: "shift_jis", to: "utf-8" } ) )
+    .pipe( postal2json() )
+    .pipe( v1() )
+    .pipe( chmod( 644 ) )
+    .pipe( gulp.dest( 'api/v1' ) );
 } );
 
+/**
+ * Create an API of the Jigyosyo postal code.
+ */
 gulp.task( 'v1-jigyosyo', function () {
-  return getZipCodeCSV( [ 'http://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip' ] )
-  .pipe( jigyosyo2json() )
-  .pipe( v1() )
-  .pipe( chmod( 644 ) )
-  .pipe( gulp.dest( 'api/v1' ) );
+  var urls = [ 'http://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip' ];
+  return download( urls )
+    .pipe( decompress() )
+    .pipe( convertEncoding( { from: "shift_jis", to: "utf-8" } ) )
+    .pipe( jigyosyo2json() )
+    .pipe( v1() )
+    .pipe( chmod( 644 ) )
+    .pipe( gulp.dest( 'api/v1' ) );
 } );
 
 gulp.task( 'default', [ 'v1', 'v1-jigyosyo' ] );
